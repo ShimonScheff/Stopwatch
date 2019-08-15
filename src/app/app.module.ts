5e5e41716629e61cf24be72d281fc924fcb80845
simons@way2vat.com <177022way2Vat>
2019-08-16 00:26:05 +0300

    Create view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HaloDirective } from './directives/halo.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HaloDirective
   ],
   imports: [
     BrowserModule,
