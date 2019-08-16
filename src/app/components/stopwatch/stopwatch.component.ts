2cd78f4f96c608d3c2a1e8af706261c9f5d940b0
ShimonScheff <shimsh16@gmail.com>
2019-08-17 01:30:01 +0300

    finish

--- src/app/components/stopwatch/stopwatch.component.ts
+++ src/app/components/stopwatch/stopwatch.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ClearAll, PauseStopwatch, SaveTimeFrame, StopWatchInterval} from '../../actions/stopwatch.action';
 import {Store} from '@ngxs/store';
 import {Observable} from 'rxjs';
