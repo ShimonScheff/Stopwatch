d5a91a5766e01358ea3ee7f33566387eb062bb1c
simons@way2vat.com <177022way2Vat>
2019-08-16 03:58:46 +0300

    All works except state management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HaloDirective } from './directives/halo.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HaloDirective
   ],
   imports: [
     BrowserModule,
